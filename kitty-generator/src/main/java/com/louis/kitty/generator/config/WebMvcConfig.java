4d95c017cc17f12e32eeedb494d9acdb6c62ca8a
eason <1161588342@qq.com>
2019-06-10 18:03:59 +0800

    first generatory

---
+++ kitty-generator/src/main/java/com/louis/kitty/generator/config/WebMvcConfig.java
@@ -0,0 +1,24 @@
+package com.louis.kitty.generator.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index.html");
+    }
+
+    /*@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+        super.addResourceHandlers(registry);
+    }*/
+}
---
+++ kitty-generator/src/main/java/com/louis/kitty/generator/controller/UiController.java
@@ -0,0 +1,26 @@
+package com.louis.kitty.generator.controller;
+
+import com.louis.kitty.core.http.HttpResult;
+import com.louis.kitty.dbms.vo.ConnParam;
+import com.louis.kitty.generator.service.GenerateService;
+import com.louis.kitty.generator.vo.GenerateModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 代码生成控制器
+ * @author Louis
+ * @date Nov 9, 2018
+ */
+@Controller
+@RequestMapping
+public class UiController {
+
+	/*@GetMapping("/")
+	public String index() {
+		return "index";
+	}*/
+
+
+}
