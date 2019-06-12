d0b4e3579d8319143abb898e6a00e3b1660407b8
eason <1161588342@qq.com>
2019-06-12 12:27:15 +0800

    修复静态资源范围问题

--- kitty-generator/src/main/java/com/louis/kitty/generator/config/WebMvcConfig.java
+++ kitty-generator/src/main/java/com/louis/kitty/generator/config/WebMvcConfig.java
@@ -2,7 +2,6 @@ package com.louis.kitty.generator.config;
 
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
@@ -15,10 +14,10 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         registry.addViewController("/").setViewName("index.html");
     }
 
-    /*@Override
+    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/");
         super.addResourceHandlers(registry);
-    }*/
+    }
 }
